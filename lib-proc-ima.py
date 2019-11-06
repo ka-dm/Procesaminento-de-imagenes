2f6d7bc1b26528aec94a18f4bc8635dfa1c5c828
Kevin <kevindm181998@gmail.com>
2019-11-06 17:39:39 -0500

    correccion del metodo convRGVtoYUV(img)

--- lib-proc-ima.py
+++ lib-proc-ima.py
@@ -28,35 +28,28 @@ def creaDirSalida():
         print("Se creo el directorio /"+ nombre)
         return(""+ script_dir + "/" + nombre)   
    
-def convRGVtoYUV(im):
-    tiempoIn = time.time()
-    ruta = ("C:/Users/kevin/Desktop/" + im)
-    im = Image.open(ruta)
-    im.show()
-    
-    im2 = im
+def convRGVtoYUV(img):
     i = 0
-    while i < im2.size[0]:
+    while i < img.size[0]:
         j = 0
-        while j < im2.size[1]:
-            r, g, b = im2.getpixel((i,j))
+        while j < img.size[1]:
+            r, g, b = img.getpixel((i,j))
             #g = (r + g + b) / 3
             #gris = int(g)
             y = 0.299*r + 0.587*g + 0.114*b
             u = -0.147*r - 0.289*g + 0.436*b
             v = 0.615*r - 0.515*g - 0.100*b
             pixel = tuple([int(y), int(u), int(v)])
-            im2.putpixel((i,j), pixel)
+            img.putpixel((i,j), pixel)
             j+=1
         i+=1
-    im2.show()
-    
-    tiempoFin = time.time()
-    print('El Proceso Tardo: ', tiempoFin - tiempoIn, 'Segundos')   
+    return img
+      
 
 if __name__ == "__main__":
     
     #Ejecucion de los metodos que lee, muestra y guarda una imagen
     imagen = leeImg("C:/Users/kevin/Documents/Procesamiento-de-imagenes/images/21_training.tif")
     muestaImg(imagen)
+    copia = convRGVtoYUV(imagen)
     escribeImg(imagen, "copia2", "png")
