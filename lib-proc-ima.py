ecb1904b7060ada812330ce98bdcf78efbe4df16
Kevin <kevindm181998@gmail.com>
2019-11-06 17:25:05 -0500

    lee, muestra y escribe una imagen (correcciones)

--- lib-proc-ima.py
+++ lib-proc-ima.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import time
+import os
 
 def leeImg(src):
     img = Image.open(src)
@@ -8,9 +9,24 @@ def leeImg(src):
 def muestaImg(img):
     img.show()
 
-def escribeImg(img):
-    img.save("C:/Users/kevin/Documents/Procesamiento-de-imagenes/granja-copia.jpg", "png")
-    #print("El nombre de la imagen es: " + img.filename)   
+def escribeImg(img, nombre, formato):
+    src_salida = creaDirSalida()
+    img.save(src_salida +"/"+ nombre + "." + formato)
+    print("Se guardo la imagen con el nombre: " + nombre + ", src: " + src_salida)
+
+def creaDirSalida():
+    # Crea un directorio con el nombre /img-salida, si no existe y retorna la ruta de la carpeta creada
+
+    script_dir = os.path.dirname(__file__) #ruta absoluta del script
+    nombre = "img-salida"
+    try:
+        os.stat(script_dir + "/" + nombre)
+        print("El directorio de salida /"+ nombre +" ya existe.") 
+        return(""+ script_dir + "/" + nombre) 
+    except:
+        os.mkdir(script_dir + "/img-salida")
+        print("Se creo el directorio /"+ nombre)
+        return(""+ script_dir + "/" + nombre)   
    
 def convRGVtoYUV(im):
     tiempoIn = time.time()
@@ -39,6 +55,8 @@ def convRGVtoYUV(im):
     print('El Proceso Tardo: ', tiempoFin - tiempoIn, 'Segundos')   
 
 if __name__ == "__main__":
-    imagen = leeImg("C:/Users/kevin/Desktop/granja.png")
-    #muestaImg(imagen)
-    escribeImg(imagen)
+    
+    #Ejecucion de los metodos que lee, muestra y guarda una imagen
+    imagen = leeImg("C:/Users/kevin/Documents/Procesamiento-de-imagenes/images/21_training.tif")
+    muestaImg(imagen)
+    escribeImg(imagen, "copia2", "png")
